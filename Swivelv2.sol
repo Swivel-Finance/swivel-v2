f23fe93859ab4adcbda69240e96beeeba650cf41
Julian Traversa <62613746+JTraversa@users.noreply.github.com>
2021-03-15 22:22:27 -0400

    Update Swivelv2.sol

--- Swivelv2.sol
+++ Swivelv2.sol
@@ -516,36 +516,3 @@ contract Swivel {
     _;
   }
 }
-        uint256 maturity_,
-        address underlying_,
-        string memory name,
-        string memory symbol
-    ) ERC20Permit(name, symbol) {
-        
-        require(maturity_ > block.timestamp && maturity_ < block.timestamp + MAX_TIME_TO_MATURITY, "Invalid maturity");
-        
-        maturity = maturity_;
-        
-        underlying = underlying_;
-        
-        admin = msg.sender;
-    }
-    
-    function burn(address from, uint256 xTokenAmount) public returns (uint256){
-        require(msg.sender == admin);
-        
-        // burn from the imported ERC20Permit
-        _burn(from, xTokenAmount);
-
-        return xTokenAmount;
-    }
-
-
-
-    function mint(address to, uint256 fyDaiAmount) public {
-        require(msg.sender == admin);
-        _mint(to, fyDaiAmount);
-    }
-
-
-}
